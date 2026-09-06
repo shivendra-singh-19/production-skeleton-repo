8326cc0b4742283dfb75997189610c6f5d67e45b
shivendra-singh-19 <shivendra.singh.3025@gmail.com>
2026-09-06 21:29:19 +0530

    Add: added tls check for redis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -24,6 +24,9 @@ import { RedisService } from './redis.service';
           keyPrefix: config.get<string>('redis.keyPrefix', ''),
           lazyConnect: false,
           maxRetriesPerRequest: 3,
+          tls: {
+            servername: config.get('redis.serverUrl'),
+          },
         });
 
         client.on('connect', () =>
